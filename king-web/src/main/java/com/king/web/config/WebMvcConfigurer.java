c3d58bb99aa897ad8740dd0fe9eb581542361935
gang.zou <gang.zou@hualongdata.com>
2017-03-21 16:38:47 +0800

    优化

--- king-web/src/main/java/com/king/web/config/WebMvcConfigurer.java
+++ king-web/src/main/java/com/king/web/config/WebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.king;
+package com.king.web.config;
 
 import com.king.auth.AuthInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- *
+ * web配置类
  * 2016.12.26
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
